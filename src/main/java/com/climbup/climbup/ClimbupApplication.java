a14dbaf09abb1475a0341fd33da0d35b39fe77c9
eric-jy-park <2019147551@yonsei.ac.kr>
2025-07-14 17:38:44 +0900

    Initial commit

---
+++ src/main/java/com/climbup/climbup/ClimbupApplication.java
@@ -0,0 +1,13 @@
+package com.climbup.climbup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimbupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/climbup/climbup/ClimbupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climbup.climbup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
