46bc070bff40466aad90e03ebb885cf03df08234
1117mg <lee_yerim@naver.com>
2025-07-26 16:48:37 +0900

    chore: oauth2 controller tag 명시

--- src/main/java/com/climbup/climbup/auth/controller/OAuth2RedirectController.java
+++ src/main/java/com/climbup/climbup/auth/controller/OAuth2RedirectController.java
@@ -1,5 +1,7 @@
 package com.climbup.climbup.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Tag(name = "OAuth2 Redirect", description = "암장 관련 API")
 @RestController
 @Slf4j
 public class OAuth2RedirectController {
 
+    @Operation(summary = "oauth2 리다이렉트 경로", description = "로그인 성공 후 리다이렉트되는 경로입니다")
     @GetMapping("/oauth2/redirect")
     public ResponseEntity<Map<String, Object>> handleOAuth2Redirect(@RequestParam String token) {
         log.info("JWT 토큰 발급 완료");
