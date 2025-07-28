1034fad8cf1b43532040f70e6da2fbeea4ff9a78
1117mg <lee_yerim@naver.com>
2025-07-29 01:25:36 +0900

    feat: 특정 암장 레벨 정보 조회 API

--- src/main/java/com/climbup/climbup/gym/service/GymServiceImpl.java
+++ src/main/java/com/climbup/climbup/gym/service/GymServiceImpl.java
@@ -1,13 +1,9 @@
 package com.climbup.climbup.gym.service;
 
-import com.climbup.climbup.brand.repository.BrandRepository;
 import com.climbup.climbup.gym.dto.response.GymResponse;
-import com.climbup.climbup.gym.dto.response.GymLevelResponse;
 import com.climbup.climbup.gym.entity.ClimbingGym;
-import com.climbup.climbup.gym.entity.GymLevel;
 import com.climbup.climbup.gym.exception.GymNotFoundException;
 import com.climbup.climbup.gym.repository.ClimbingGymRepository;
-import com.climbup.climbup.gym.repository.GymLevelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,8 +15,6 @@ import java.util.List;
 public class GymServiceImpl implements GymService {
 
     private final ClimbingGymRepository climbingGymRepository;
-    private final GymLevelRepository gymLevelRepository;
-    private final BrandRepository brandRepository;
 
     @Override
     @Transactional(readOnly = true)
@@ -37,15 +31,6 @@ public class GymServiceImpl implements GymService {
         ClimbingGym gym = climbingGymRepository.findByIdWithBrand(gymId)
                 .orElseThrow(() -> new GymNotFoundException());
         return GymResponse.fromEntity(gym);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<GymResponse> getGymsByBrandName(String brandName) {
-        List<ClimbingGym> gyms = climbingGymRepository.findByBrandNameWithBrand(brandName);
-        return gyms.stream()
-                .map(GymResponse::fromEntity)
-                .toList();
     }
 
     @Override
