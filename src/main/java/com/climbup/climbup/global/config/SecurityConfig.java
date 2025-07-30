0e61e767cb04b6d76302854c0479fbaeb97e0de4
1117mg <lee_yerim@naver.com>
2025-07-30 22:06:47 +0900

    chore: allow preflight request

--- src/main/java/com/climbup/climbup/global/config/SecurityConfig.java
+++ src/main/java/com/climbup/climbup/global/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/", "/api/auth/refresh", "/login/**", "/oauth2/**",
                                 "/login/oauth2/code/*", "/error", "/css/**", "/js/**",
                                 "/images/**", "/favicon.ico").permitAll()
